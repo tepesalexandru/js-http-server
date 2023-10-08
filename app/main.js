738659064db9a3c9fc9255849f776bef2e3709b5
Tepes Alexandru <tepes_alexandru@outlook.com>
2023-10-08 17:44:00 +0300

    .

--- app/main.js
+++ app/main.js
@@ -8,9 +8,25 @@ const server = net.createServer((socket) => {
   // accept tcp connection
   socket.on("data", (data) => {
     console.log("data", data.toString());
-    // respond with a HTTP 200 OK
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    socket.end();
+    // extract data from the buffer
+    const dataString = data.toString();
+    // extract all data by separating by new line
+    const dataArr = dataString.split("\r\n");
+    // extract the first line
+    const firstLine = dataArr[0];
+    // extract the method
+    const method = firstLine.split(" ")[0];
+    // extract the path
+    const path = firstLine.split(" ")[1];
+
+    // if the path is / then return HTTP 200 OK
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      socket.end();
+      return;
+    }
+    // otherwise, rerturn HTTP 404 Not Found
+    socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
   });
   socket.on("close", () => {
     socket.end();
