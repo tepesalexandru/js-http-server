07d3cc2da163eb16a043142f6976d8c81a1d51c9
Tepes Alexandru <tepes_alexandru@outlook.com>
2023-10-08 17:33:09 +0300

    com

--- app/main.js
+++ app/main.js
@@ -5,6 +5,11 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  // accept tcp connection
+  socket.on("data", (data) => {
+    // respond with a HTTP 200 OK
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  });
   socket.on("close", () => {
     socket.end();
     server.close();
